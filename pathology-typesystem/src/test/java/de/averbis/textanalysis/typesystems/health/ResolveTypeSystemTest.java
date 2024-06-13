25a7f68e48f8c30faa5731d6b9f0485681f34534
Theresa Klumpp <theresa.klumpp@averbis.com>
2024-06-13 11:15:14 +0200

    Issue #68: Extend PathologyDocumentClassification/PathologyDocumentation with features representing punches
    - Add "PunchIdentifier"

--- pathology-typesystem/src/test/java/de/averbis/textanalysis/typesystems/health/ResolveTypeSystemTest.java
+++ pathology-typesystem/src/test/java/de/averbis/textanalysis/typesystems/health/ResolveTypeSystemTest.java
@@ -29,6 +29,6 @@ class ResolveTypeSystemTest {
 		TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription(
 				"de.averbis.textanalysis.typesystems.health.PathologyTypeSystem");
 		tsd.resolveImports();
-		assertThat(tsd.getTypes()).hasSize(226);
+		assertThat(tsd.getTypes()).hasSize(227);
 	}
 }
