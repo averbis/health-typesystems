b305cc3a35fd0f48092708c8f288b92b1cf93ff6
Viorel Morari <viorel.morari@averbis.com>
2024-06-25 15:19:32 +0200

    Issue #72: extend clinical section typesystem with new types
    - extend typesystem
    - adjust tests

--- clinical-section-typesystem/src/test/java/de/averbis/textanalysis/typesystems/health/ResolveTypeSystemTest.java
+++ clinical-section-typesystem/src/test/java/de/averbis/textanalysis/typesystems/health/ResolveTypeSystemTest.java
@@ -29,6 +29,6 @@ class ResolveTypeSystemTest {
 		TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription(
 				"de.averbis.textanalysis.typesystems.health.ClinicalSectionTypeSystem");
 		tsd.resolveImports();
-		assertThat(tsd.getTypes()).hasSize(99);
+		assertThat(tsd.getTypes()).hasSize(102);
 	}
 }
